9c1331447cbc800b1d50473e7343e26a216c2bdc
Alberto <better.acs@gmail.com>
2013-12-07 18:12:04 +0100

    Animacion de muerte (casi) completa.

--- Assets/Scripts/PlayerDeath.js
+++ Assets/Scripts/PlayerDeath.js
@@ -1,3 +1,30 @@
+private var dying : boolean;
+private var jumpSpeed : float;
+private var fallSpeed : float = 2.0;
+private var t : float = 0.85;
+var movCameraScript : movimientoCamara;
+var boxCollider : BoxCollider2D;
+var circleCollider : CircleCollider2D;
+var rightCollider : BoxCollider2D;
+
+function Start(){
+	dying = false;
+	jumpSpeed = 70.0;
+}
+
+function Update(){
+	if(dying){
+		var rig : Rigidbody2D = GetComponent(Rigidbody2D);
+		var movement = Vector2(0, jumpSpeed);
+		jumpSpeed *= t;
+		rig.AddForce(movement);
+	}
+}
+
 function die (){
+	dying = true;
+	movCameraScript.stopCamera();
+	boxCollider.enabled = false;
+	circleCollider.enabled = false;
 	print("He muerto!! :'(");
 }
--- Assets/Scripts/movimientoCamara.js
+++ Assets/Scripts/movimientoCamara.js
@@ -1,8 +1,22 @@
 ﻿#pragma strict
 
 var prueba : float =5;
+var running : boolean;
+
+function Start(){
+	running = true;
+}
 
 function Update () {
-
-	transform.Translate(Vector3(1,0,0) * Time.deltaTime*prueba);
+	if(running){
+		transform.Translate(Vector3(1,0,0) * Time.deltaTime*prueba);
+	}
 } 
+
+function stopCamera(){
+	running = false;
+}
+
+function resumeCamera(){
+	running = true;
+}
