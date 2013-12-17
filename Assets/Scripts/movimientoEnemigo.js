e4b703f6d944b9f3540ff3534e11fb8d5bb1a6d0
Yarrick <potrayarrick@gmail.com>
2013-12-17 13:22:52 +0100

    Arregladas colisiones entre enemigos y la cámara.
    - Añadidas layers para la cámara y el enemigo.
    - Cambiada variable "velocity" por "speed" (Jahiel tio...)
    - Todo metido en las layers correspondientes.
    - Ignoradas colisiones entre layers 9 y 10.

--- Assets/Scripts/movimientoEnemigo.js
+++ Assets/Scripts/movimientoEnemigo.js
@@ -1,32 +1,32 @@
-﻿#pragma strict
+#pragma strict
 
-var velocity :float;
+var speed :float;
 var posMinX :float;
 var posMaxX :float;
 
 function Start () {
-
+	
 	posMaxX = this.gameObject.transform.position.x;
 	posMinX = posMaxX - 30.0;
-	velocity = 3;
+	speed = 3;
 }
 
 function Update () {
 	
 	if(this.gameObject.transform.position.x > posMaxX || this.gameObject.transform.position.x < posMinX)
-		velocity *= -1;
+		speed *= -1;
 	
-	this.gameObject.transform.Translate(Vector3(velocity, 0, 0)*Time.deltaTime);    
+	this.gameObject.transform.Translate(Vector3(speed, 0, 0)*Time.deltaTime);
 	//this.gameObject.transform.position.x += velocity;
 	
 	//var movement = Vector2(velocity, 0);
 	//this.rigidbody2D.AddForce(movement);
-
+	
 }
 
 
 function OnCollisionEnter2D(col: Collision2D){
-
+	
 	if(col.gameObject.name == "Trampa" || col.gameObject.name == "Caja carton" || col.gameObject.tag == "Coche")
-		velocity *= -1;
+		speed *= -1;
 }
