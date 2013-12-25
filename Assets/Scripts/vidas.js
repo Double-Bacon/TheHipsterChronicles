64a7ec9baa82389a8646fd73f231d5dd6090fac9
Juan Porcel <jotas@correo.ugr.es>
2013-12-25 16:09:40 +0100

    disparos hechos

--- Assets/Scripts/vidas.js
+++ Assets/Scripts/vidas.js
@@ -17,13 +17,26 @@ function Start () {
 
 function Update () {}
 
-function OnCollisionEnter2D(coll: Collision2D) {
+//function OnCollisionEnter2D(coll: Collision2D) {
+//
+//	Debug.Log("Vidas.js--> OnCollisionEnter2D:"+coll.gameObject.name);
+//	if ((coll.gameObject.tag.Contains("Trampa") || coll.gameObject.name.Contains("Enemigo1")) && !noHarmModeScript.getEnabled()){
+//
+//		numVidas--;
+//		
+//		noHarmModeScript.startNoHarmMode();
+//	}
+//} 
 
-	
-	if ((coll.gameObject.tag == "Trampa" || coll.gameObject.name == "Enemigo1") && !noHarmModeScript.getEnabled()){
+
+function OnTriggerEnter2D (coll : Collider2D){
+	Debug.Log("Vidas.js--> OnCollisionEnter2D:"+coll.gameObject.name);
+	if ((coll.gameObject.tag.Contains("Trampa") || coll.gameObject.name.Contains("Enemigo1")) && !noHarmModeScript.getEnabled()){
 
 		numVidas--;
 		
 		noHarmModeScript.startNoHarmMode();
 	}
-} 
+	
+	
+}
